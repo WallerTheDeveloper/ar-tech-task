a755f24ac331493345617b989b297a0c80e5f8d5
Daniel <golo7ov.danil@gmail.com>
2023-07-24 22:30:16 +0200

    Portal adjustment

---
+++ Assets/Scripts/PortalController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARFoundation;
+
+public class PortalController : MonoBehaviour
+{
+    public Transform portal;
+    public ARTrackedImageManager trackedImageManager;
+    private Vector3 initialScale;
+
+    private void OnEnable()
+    {
+        trackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+    
+    private void InitPortal()
+    {
+        foreach (ARTrackedImage trackedImage in trackedImageManager.trackables)
+        {
+            if (trackedImage.tag == "Portal")
+            {
+                portal = trackedImage.transform;
+                break; // Found the portal, no need to continue the loop
+            }
+        }
+        initialScale = portal.transform.localScale;
+    }
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        InitPortal();
+        
+        foreach (ARTrackedImage trackedImage in trackedImageManager.trackables)
+        {
+            float distanceToMarker = Vector3.Distance(Camera.main.transform.position, trackedImage.transform.position);
+            portal.transform.localScale = initialScale * distanceToMarker;   
+        }
+        // Debug.Log(
+        //     $"There are {trackedImageManager.trackables.count} images being tracked.");
+        // foreach (ARTrackedImage trackedImage in eventArgs.added)
+        // {
+        //     if (trackedImage.tag == "Portal")
+        //     {
+        //         portal.gameObject.SetActive(true);
+        //     }
+        // }
+        //
+        // foreach (ARTrackedImage trackedImage in eventArgs.updated)
+        // {
+        //     if (trackedImage.tag == "Portal")
+        //     {
+        //         float distanceToMarker = Vector3.Distance(Camera.main.transform.position, trackedImage.transform.position);
+        //         portal.transform.localScale = initialScale * distanceToMarker;
+        //     }
+        // }
+        //
+        // foreach (ARTrackedImage trackedImage in eventArgs.removed)
+        // {
+        //     if (trackedImage.tag == "Portal")
+        //     {
+        //         portal.gameObject.SetActive(false);
+        //     }
+        // }
+    }
+}
