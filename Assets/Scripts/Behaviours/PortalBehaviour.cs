940eed407def904e928fb14d915dbcfe1dc89f5b
Daniel <golo7ov.danil@gmail.com>
2023-07-24 19:51:34 +0200

    Small portal rotation fix

---
+++ Assets/Scripts/Behaviours/PortalBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Behaviours
+{
+    public class PortalBehaviour : MonoBehaviour
+    {
+        private void Start()
+        {
+            transform.rotation = Quaternion.Euler(180f, 0f, 0f);
+        }
+    }
+}
