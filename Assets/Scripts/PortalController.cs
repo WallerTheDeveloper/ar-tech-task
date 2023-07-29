6c6a1b8fdb8ae0dbb2adcf95427c622d3dc64a63
Daniel <golo7ov.danil@gmail.com>
2023-07-29 21:26:25 +0200

    Portal effect reverse

--- Assets/Scripts/PortalController.cs
+++ Assets/Scripts/PortalController.cs
@@ -43,26 +43,49 @@ public class PortalController : MonoBehaviour
         if (!portalFound)
         {
             InitPortal();
-            
         }
+    
         foreach (ARTrackedImage trackedImage in eventArgs.updated)
         {
             if (trackedImage.CompareTag(_trackedObjectTag.ToString()) && trackedImage.trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Tracking)
             {
                 float distanceToMarker = Vector3.Distance(Camera.main.transform.position, trackedImage.transform.position);
-                
-                portal.localScale = -initialScale * distanceToMarker;
-                print("local scale: " + portal.localPosition);
-                print("Init scale: " + initialScale);
-                print("distance: " + distanceToMarker);
+            
+                float scaleMultiplier = 0.01f; // Adjust this value to control the scaling speed
+                float newScale = Mathf.Clamp(initialScale.x + distanceToMarker * scaleMultiplier, 1f, 10f);
+            
+                portal.localScale = new Vector3(newScale, newScale, newScale);
 
                 portal.position = trackedImage.transform.position;
-
                 portal.rotation = trackedImage.transform.rotation * Quaternion.Euler(-270f, 0f, 0f);
-                
-                Debug.Log("Image position: " + trackedImage.transform.position + "Image rotation: " + trackedImage.transform.rotation);
-                print("Position: " + portal.position + " rotation: " + portal.rotation);
             }
         }
     }
+    // void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    // {
+    //     if (!portalFound)
+    //     {
+    //         InitPortal();
+    //         
+    //     }
+    //     foreach (ARTrackedImage trackedImage in eventArgs.updated)
+    //     {
+    //         if (trackedImage.CompareTag(_trackedObjectTag.ToString()) && trackedImage.trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Tracking)
+    //         {
+    //             float distanceToMarker = Vector3.Distance(Camera.main.transform.position, trackedImage.transform.position);
+    //             
+    //             portal.localScale = -initialScale * distanceToMarker;
+    //             print("local scale: " + portal.localPosition);
+    //             print("Init scale: " + initialScale);
+    //             print("distance: " + distanceToMarker);
+    //
+    //             portal.position = trackedImage.transform.position;
+    //
+    //             portal.rotation = trackedImage.transform.rotation * Quaternion.Euler(-270f, 0f, 0f);
+    //             
+    //             Debug.Log("Image position: " + trackedImage.transform.position + "Image rotation: " + trackedImage.transform.rotation);
+    //             print("Position: " + portal.position + " rotation: " + portal.rotation);
+    //         }
+    //     }
+    // }
 }
